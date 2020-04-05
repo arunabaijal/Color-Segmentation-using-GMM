eff47d464d082a0906e2524594cd3f8695b4cef1
Aruna Baijal <aruna11baijal@gmail.com>
2020-04-05 04:16:45 -0400

    Change covariance matrix

--- GMM.py
+++ GMM.py
@@ -1,14 +1,30 @@
+#!/usr/bin/env python
 # import imageio
 # import matplotlib.animation as ani
 # import matplotlib.cm as cmx
 # import matplotlib.colors as colors
 # import matplotlib.pyplot as plt
 import numpy as np
+import cv2
+import sys
+import glob
 
 # from matplotlib.patches import Ellipse
 # from PIL import Image
 # from sklearn import datasets
 from sklearn.cluster import KMeans
+from scipy import stats
+
+
+def generate_gaussian(image_path):
+    image = cv2.imread(image_path)
+    red_pts = image[:,:,2]
+    m_r, s_r = stats.norm.fit(red_pts)
+    green_pts = image[:,:,1]
+    m_g, s_g = stats.norm.fit(green_pts)
+    blue_pts = image[:,:,0]
+    m_b, s_b = stats.norm.fit(blue_pts)
+    return [m_r, s_r, m_g, s_g, m_b, s_b]
 
 def normal_dist_3D(mean):
     mu = np.mean(mean)
@@ -29,6 +45,10 @@ def normal_dist_3D(mean):
 def gaussian(X, mu, cov):
     n = X.shape[1]
     diff = (X - mu).T
+    # print("covariance",cov)
+    # print("diff", diff)
+    # if np.linalg.det(cov) == 0:
+    #     return np.zeros(X.shape[0]).reshape(-1, 1)
     return np.diagonal(1 / ((2 * np.pi) ** (n / 2) * np.linalg.det(cov) ** 0.5) * np.exp(-0.5 * np.dot(np.dot(diff.T, np.linalg.inv(cov)), diff))).reshape(-1, 1)
 
 def initialize_clusters(X, n_clusters):
@@ -44,7 +64,7 @@ def initialize_clusters(X, n_clusters):
         clusters.append({
             'pi_k': 1.0 / n_clusters,
             'mu_k': mu_k[i],
-            'cov_k': np.identity(X.shape[1], dtype=np.float64)
+            'cov_k': np.identity(X.shape[1], dtype=np.float64)*30
         })
     
     return clusters
@@ -59,7 +79,7 @@ def expectation_step(X, clusters):
         cov_k = cluster['cov_k']
         
         gamma_nk = (pi_k * gaussian(X, mu_k, cov_k)).astype(np.float64)
-        
+        # print(gamma_nk)
         for i in range(X.shape[0]):
             totals[i] += gamma_nk[i]
         
@@ -131,3 +151,15 @@ def train_gmm(X, n_clusters, n_epochs):
     
     return clusters, likelihoods, scores, sample_likelihoods, history
 
+if __name__ == '__main__':
+    
+    for image_path in glob.glob("Data/Red/Extracted/*"):
+        image = cv2.imread(image_path)
+        X = []
+        for i in range(image.shape[1]):
+            for j in range(image.shape[0]):
+                if [image[j][i][2], image[j][i][1], image[j][i][0]] != [0,0,0]:
+                    X.append([image[j][i][2], image[j][i][1], image[j][i][0]])
+
+        clusters, likelihoods, scores, sample_likelihoods, history = train_gmm(np.array(X), 3, 50)
+    # print(X)
